0de5b821ee8ee3f4dadc459e79a0918dd995cb5e
Roee Friedman <roee.friedman@gmail.com>
2023-08-05 23:44:17 +0300

    Version 0.6 - command line arguments, now with argparse Not yet: - Just cropping, not really deleting (but file is optimized)

--- main.py
+++ main.py
@@ -5,7 +5,39 @@ from pypdf import PdfWriter, PdfReader
 import argparse
 
 
-def crop_file(path, filename, direction='V', skip=0):
+def handle_args(args):
+    error = False
+
+    if not os.path.isdir(args.path):
+        print(f'Directory not found: {args.path}')
+        error = True
+    if args.path[-1] != '/':
+        args.path += '/'
+
+    if not os.path.isfile(args.path + args.file):
+        print(f'File not found: {args.path + args.file}')
+        error = True
+
+    if error:
+        sys.exit(1)
+
+
+def optimize_file(file):
+    # Sometimes reading and writing will compress the file
+    print('Optimizing')
+    reader = PdfReader(file)
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        writer.add_page(page)
+
+    writer.add_metadata(reader.metadata)
+
+    with open(file+'_compressed.pdf', "wb") as fp:
+        writer.write(fp)
+
+
+def crop_file(path, filename, direction='V', skip=0, optimize=False):
     # open input file, PdfReader
     input_pdf_file = PdfReader(open(path+filename, 'rb'))
     # create PdfWriter
@@ -39,58 +71,18 @@ def crop_file(path, filename, direction='V', skip=0):
     with open(path + filename + '_final.pdf', 'wb') as output_file:
         output_pdf.write(output_file)
 
-    # Sometimes reading and writing will compress the file
-    # reader = PdfReader(PATH+FILE+'_final.pdf')
-    # writer = PdfWriter()
-    #
-    # for page in reader.pages:
-    #     writer.add_page(page)
-    #
-    # writer.add_metadata(reader.metadata)
-    #
-    # with open(PATH+FILE+'_compressed.pdf', "wb") as fp:
-    #     writer.write(fp)
-
-
-def handle_args(args):
-    error = False
-    if args.path is None:
-        args.path = './'
-    if not os.path.isdir(args.path):
-        print(f'Directory not found: {args.path}')
-        error = True
-    if args.path[-1] != '/':
-        args.path += '/'
-
-    if args.file is None:
-        print('File not found: None')
-        error = True
-    if not os.path.isfile(args.path + args.file):
-        print(f'File not found: {args.path + args.file}')
-        error = True
-
-    if args.skip_page is None:
-        args.skip_page = '0'
-    if not args.skip_page.isnumeric():
-        print(f'skip_page should be a number: {args.skip_page}')
-        error = True
-
-    if args.direction is None:
-        args.direction = 'v'
-    if args.direction.lower() != 'v' and args.direction.lower() != 'h':
-        print(f'direction can be v or h: {args.direction}')
-        error = True
-
-    if error:
-        sys.exit(1)
+    if optimize:
+        optimize_file(path+filename+'_final.pdf')
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--path', help='File path')
-parser.add_argument('-f', '--file', help='File name')
-parser.add_argument('-d', '--direction', help='Direction of cropping (v|h)')
-parser.add_argument('-s', '--skip_page', help='number of first pages to skip (default = 0)')
+parser.add_argument('-p', '--path', help='File path', default='./')
+parser.add_argument('-f', '--file', help='File name', required=True)
+parser.add_argument('-d', '--direction', help='Direction of cropping (v|h)', choices=['v', 'h'], default='v')
+parser.add_argument('-s', '--skip_page', help='number of first pages to skip (default = 0)', default=0, type=int)
+parser.add_argument('-o', '--optimize', help='Attempt to optimize file size (default = False)', default=False,
+                    action='store_true')
 cl_args = parser.parse_args()
 handle_args(cl_args)
 
-crop_file(cl_args.path, cl_args.file, cl_args.direction.lower(), int(cl_args.skip_page))
+crop_file(cl_args.path, cl_args.file, cl_args.direction.lower(), int(cl_args.skip_page), cl_args.optimize)
